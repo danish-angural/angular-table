d91713a98b516a51a9a293e17c5012cc574a72ac
danish-angural <danishangural28@gmail.com>
2020-05-26 17:51:41 +0530

    styling

--- src/app/components/usertable/usertable.component.ts
+++ src/app/components/usertable/usertable.component.ts
@@ -1,17 +1,21 @@
-import { MatTableDataSource } from '@angular/material/table';
+import { User } from './../../models/user.models';
 import { MatPaginator } from '@angular/material/paginator';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { Observable } from 'rxjs';
 import {DataSource} from '@angular/cdk/collections';
-import { User } from '../../models/user.models';
 import { MatSort } from '@angular/material/sort';
+import { style } from '@angular/animations';
 @Component({
   selector: 'usertable',
   templateUrl: './usertable.component.html',
   styleUrls: ['./usertable.component.css']
 })
 export class UsertableComponent implements OnInit {
+  name: String="";
+  roll: number=0;
+  id:String="";
+
   dataSource = new UserDataSource(this.userService);
   displayedColumns = ['name', '_id', 'roll'];
   @ViewChild(MatPaginator) paginator: MatPaginator;    
@@ -19,7 +23,15 @@ export class UsertableComponent implements OnInit {
   
   ngOnInit() {
   }
-}export class UserDataSource extends DataSource<any> {
+  getRecord(user: User){
+    let elem: HTMLElement = document.getElementById('modal');
+    this.name=user.name;
+    this.id=user._id;
+    this.roll=user.roll;
+    elem.setAttribute("style", "display: block;")
+  }
+}
+export class UserDataSource extends DataSource<any> {
   constructor(private userService: UserService) {
     super();
   }
